d18fbacbcae80c8f859c0157e32be7f18f7afa52
Andrei Konovalov <a.konovaloff13@yandex.com>
2025-03-20 19:05:05 +0400

    Removed a class method

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -3,34 +3,41 @@ from api.stellar_burger_api import StellarBurgerApi
 from conftest import new_user, existing_user, not_valid_pass_user, no_password_user
 import allure
 
-class TestLoginUser:
 
-    @classmethod
-    def login_user(cls, payload, expected_status, expected_message):
-        """Отправляет запрос на вход с разными данными пользователя и проверяет ответ сервера
-        """
-        response = StellarBurgerApi.login_user(payload)
-        assert response.status_code == expected_status, "\nКод ответа сервера не соответствует ожидаемому"
-        assert expected_message in response.text, (
-            f"\nТекст ответа сервера не содержит ожидаемую часть '{expected_message}'"
-            f"\nОтвет сервера: {response.text}")
+class TestLoginUser:
 
     @allure.title("Вход не зарегистрированного пользователя")
     def test_login_user_new_user(self, new_user):
         """Логин нового не зарегистрированного пользователя"""
-        self.login_user(new_user, 401, "email or password are incorrect")
+        response = StellarBurgerApi.login_user(new_user)
+        assert response.status_code == 401, "\nКод ответа сервера не соответствует ожидаемому"
+        assert "email or password are incorrect" in response.text, (
+            f"\nТекст ответа сервера не содержит ожидаемую часть 'email or password are incorrect'"
+            f"\nОтвет сервера: {response.text}")
 
     @allure.title("Вход зарегистрированного пользователя")
     def test_login_user_existing_user(self, existing_user):
         """Логин существующего пользователя"""
-        self.login_user(existing_user, 200, '"success":true')
+        response = StellarBurgerApi.login_user(existing_user)
+        assert response.status_code == 200, "\nКод ответа сервера не соответствует ожидаемому"
+        assert '"success":true' in response.text, (
+            f"\nТекст ответа сервера не содержит ожидаемую часть 'email or password are incorrect'"
+            f"\nОтвет сервера: {response.text}")
 
     @allure.title("Попытка входа с неправильным паролем")
     def test_login_user_existing_user_but_incorrect_password(self, not_valid_pass_user):
         """Логин существующего пользователя с некорректным паролем"""
-        self.login_user(not_valid_pass_user, 401, "email or password are incorrect")
+        response = StellarBurgerApi.login_user(not_valid_pass_user)
+        assert response.status_code == 401, "\nКод ответа сервера не соответствует ожидаемому"
+        assert "email or password are incorrect" in response.text, (
+            f"\nТекст ответа сервера не содержит ожидаемую часть 'email or password are incorrect'"
+            f"\nОтвет сервера: {response.text}")
 
     @allure.title("Вход пользователя без поля 'пароль' в запросе")
     def test_login_user_existing_user_but_no_password_field(self, no_password_user):
         """Логин существующего пользователя без поля "пароль" в запросе"""
-        self.login_user(no_password_user, 401, "email or password are incorrect")
+        response = StellarBurgerApi.login_user(no_password_user)
+        assert response.status_code == 401, "\nКод ответа сервера не соответствует ожидаемому"
+        assert "email or password are incorrect" in response.text, (
+            f"\nТекст ответа сервера не содержит ожидаемую часть 'email or password are incorrect'"
+            f"\nОтвет сервера: {response.text}")
